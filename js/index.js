f3627e0adb5c4760e9108c59cc7e73937f486657
Aurore-Grimault <auroregrimault@hotmail.fr>
2022-04-26 09:24:10 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,145 @@
+// function swap(cssFile) {
+//     let link = document.getElementById("change-style")
+//     link.href = cssFile;
+// }
+
+
+let reset = document.querySelector("#reset");
+
+reset.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlereset.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/reset.css');
+    }
+}
+let square = document.querySelector("#square");
+
+square.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlesquare.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/square.css');
+    }
+}
+
+let cat = document.querySelector("#cat");
+
+cat.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlecat.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/cat.css');
+    }
+}
+
+let bear = document.querySelector("#bear");
+
+bear.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlebear.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/bear.css');
+    }
+}
+
+let fish = document.querySelector("#fish");
+
+fish.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlefish.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/fish.css');
+    }
+}
+
+let goose = document.querySelector("#goose");
+
+goose.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlegoose.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/goose.css');
+    }
+}
+
+let dog = document.querySelector("#dog");
+
+dog.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littledog.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/dog.css');
+    }
+}
+
+let bird = document.querySelector("#bird");
+
+bird.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlebird.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/AdrienBird.css');
+    }
+}
+let turtle = document.querySelector("#turtle");
+
+turtle.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littleturtle.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/AuroreTurtle.css');
+    }
+}
+let frog = document.querySelector("#frog");
+
+frog.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlefrog.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/FloFrog.css');
+    }
+}
+let vulture = document.querySelector("#vulture");
+
+vulture.onclick = function swap(cssFile) {
+    if (window.matchMedia("(max-width: 767px)").matches){
+        let link = document.getElementById("change-style");
+        link.href = ('css/littlevulture.css');
+    }
+    else {
+        let link = document.getElementById("change-style");
+        link.href = ('css/CedricVulture.css');
+    }
+}
+
